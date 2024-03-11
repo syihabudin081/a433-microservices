952132ea4acac3e0d341f94f40a3ec174321b216
syihabudin081 <syihabupnyk@gmail.com>
2024-03-12 03:14:53 +0700

    show images on local

--- build_push_image.sh
+++ build_push_image.sh
@@ -3,14 +3,17 @@
 # Load environment variables from .env file
 source .env
 
-# Build Docker image
+# Perintah untuk membuat Docker image dari Dockerfile dengan nama item-app dan tag v1
 docker build -t item-app:v1 .
 
-# Tag Docker image
+# Melihat daftar image di lokal
+docker images
+
+# Mengubah nama image agar sesuai dengan format Docker Hub
 docker tag item-app:v1 "$DOCKER_USERNAME/item-app:v1"
 
-# Login to Docker Hub
+# Login ke Docker Hub
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-# Push Docker image to Docker Hub
+# Mengunggah image ke Docker Hub
 docker push "$DOCKER_USERNAME/item-app:v1"
