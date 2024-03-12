1fd6a18da6c23d5c00613ef82e3403105bb62d2b
syihabudin081 <syihabupnyk@gmail.com>
2024-03-13 02:27:21 +0700

    add shells cript

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Load environment variables from .env file
+source .env
+
+# Perintah untuk membuat Docker image dari Dockerfile dengan nama karsajobs dan tag latest
+docker build -t ngabtapir/karsajobs:latest .
+
+# Login ke Docker Hub
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+# Mengunggah image ke Docker Hub
+docker push ngabtapir/karsajobs:latest
+
+
