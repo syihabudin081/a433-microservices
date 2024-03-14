43bee66c07b4a1c48de42a90c954227fee493bdc
Syihabudin Rahmat Ramadhan <99803288+syihabudin081@users.noreply.github.com>
2024-03-15 00:22:12 +0700

    add docker and shells cript

---
+++ build_push_image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Load environment variables from .env file
+source .env
+
+# Set the image name with GHCR prefix (replace 'username' with your actual username)
+IMAGE_NAME=ghcr.io/syihabudin081/a433-microservices/shipping-service:latest
+
+docker build -t "$IMAGE_NAME" .
+
+# Login to GHCR using personal access token with Docker permissions
+echo "$GH_TOKEN" | docker login ghcr.io -u syihabudin081 --password-stdin
+
+# Push the image to GHCR
+docker push "$IMAGE_NAME"
